7628be9d2b6b152cb252dfa7687b2e7ef42725bf
Kunal Soni <117652942+Sonikuna818@users.noreply.github.com>
2023-11-28 14:41:06 +0530

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,98 @@
+package com.database.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.database.entity.Employee;
+import com.database.service.EmpService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController
+{
+	@Autowired
+	private EmpService empService;
+	
+	@GetMapping("/")
+  public String index(Model m)
+  { 
+		List<Employee> list=empService.getAllEmp();
+		m.addAttribute("empList", list);
+		return "index";
+	  
+  }
+	
+	@GetMapping("/loadEmpSave")
+	  public String loadEmpSave()
+	  { 
+			return "emp_save";
+		
+	  }
+	
+	@GetMapping("/EditEmp/{id}")
+	  public String EditEmp(@PathVariable int id, Model m)
+	  { 
+		//System.out.println(id);
+		Employee emp=empService.getEmpById(id);
+		m.addAttribute("emp",emp);
+			return "edit_emp";
+		  
+	  }
+	
+	@PostMapping("/saveEmp")
+	public String saveEmp(@ModelAttribute Employee emp,HttpSession session)
+	{
+		//System.out.println(emp);
+	Employee newEmp =	empService.saveEmp(emp);
+	
+	if(newEmp!=null) 
+	{
+		//System.out.println("save success");
+		session.setAttribute("msg", "Register Succesfully");
+	}else
+	{
+		session.setAttribute("msg", "Something wrong on server");
+	}
+		return "redirect:/loadEmpSave";
+	}
+	
+	@PostMapping("/updateEmpDtls")
+	public String updateEmp(@ModelAttribute Employee emp,HttpSession session)
+	{
+		//System.out.println(emp);
+	Employee updateEmp =	empService.saveEmp(emp);
+	
+	if(updateEmp!=null) 
+	{
+		//System.out.println("save success");
+		session.setAttribute("msg", "Update Succesfully");
+	}else
+	{
+		session.setAttribute("msg", "Something wrong on server");
+	}
+		return "redirect:/";
+	}
+	@GetMapping("/deleteEmp/{id}")
+	  public String loadEmpSave(@PathVariable int id, HttpSession session)
+	  { 
+		boolean f=empService.deleteEmp(id);
+		if(f) {
+			session.setAttribute("msg", "Delete Successfully");
+		}
+		else
+		{
+			session.setAttribute("msg", "Something went wrong");
+		}
+		return "redirect:/";
+		
+	  }
+	
+}
